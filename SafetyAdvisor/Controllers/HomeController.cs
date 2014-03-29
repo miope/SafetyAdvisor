21f7c27650df8e1a6953ced433a055b55e870883
Miodrag Perunicic <pmiodrag@gmail.com>
2014-03-29 22:18:54 +0100

    implemented contact form and sending emails to admin (configurable in web.config)

--- SafetyAdvisor/Controllers/HomeController.cs
+++ SafetyAdvisor/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SafetyAdvisor.Models;
+using SafetyAdvisor.Helpers;
+using System.Net.Mail;
+using System.Configuration;
 
 namespace SafetyAdvisor.Controllers
 {
@@ -26,6 +30,32 @@ namespace SafetyAdvisor.Controllers
         public ActionResult Contact()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                string _toEmail = ConfigurationManager.AppSettings["SafetyAdvisor.AdminEmailAddress"];
+                MailMessage _emailMessage = new MailMessage(model.Email, _toEmail);
+                _emailMessage.Subject = string.Format("SafetyAdvisor: Contact request from {0}", model.Email);
+                _emailMessage.Body = model.GetContentsForEmail();
+
+                SmtpClient _smtpClient = new SmtpClient();
+                try
+                {
+                    _smtpClient.Send(_emailMessage);
+                }
+                catch (Exception ex)
+                {
+                    return View().Alert(AlertType.Danger, string.Format("Message could not be sent: {0}", ex.Message));
+                }
+
+                return View().Alert(AlertType.Success, "Thank you for your inquiry. We will be getting back to you as soon as we can.");
+            }
+            return View(model);
         }
     }
 }
---
+++ SafetyAdvisor/Models/ContactFormModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SafetyAdvisor.Models
+{
+    public class ContactForm
+    {
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        [Required]
+        public string Message { get; set; }
+
+        public string GetContentsForEmail()
+        {
+            string _emailContent = string.Empty;
+            // some time later we will load this from a text file template
+            // for now let's hardcode it here...
+            _emailContent = string.Format("Name: {0}\r\nEmail: {1}\r\nPhone: {2}\r\n\r\nMessage:\r\n{3}", 
+                    this.Name,
+                    this.Email,
+                    this.Phone,
+                    this.Message
+                );
+
+            return _emailContent;
+        }
+    }
+}
