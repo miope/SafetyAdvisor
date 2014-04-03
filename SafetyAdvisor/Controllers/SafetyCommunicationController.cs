6203828b65580834c4aee7ad78e262f2b08425b9
Miodrag Perunicic <pmiodrag@gmail.com>
2014-04-03 23:47:58 +0200

    renamed communication into safetycommunication as it is in Micha's document

---
+++ SafetyAdvisor/Controllers/SafetyCommunicationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SafetyAdvisor.Controllers
+{
+    [Authorize]
+    public class SafetyCommunicationController : Controller
+    {
+        //
+        // GET: /Communication/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
