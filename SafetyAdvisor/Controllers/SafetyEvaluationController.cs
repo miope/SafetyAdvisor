866de84a295677a3fc37d4f10025bd21ab6e6dff
Miodrag Perunicic <pmiodrag@gmail.com>
2014-04-03 13:24:04 +0200

    started working on safety-evalution page

---
+++ SafetyAdvisor/Controllers/SafetyEvaluationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SafetyAdvisor.Controllers
+{
+    [Authorize]
+    public class SafetyEvaluationController : Controller
+    {
+        //
+        // GET: /SafetyEvaluation/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
